85cb45c8cd9ec65b37800d3f2e1ae13ed78bb8e4
JuiceMSW <juicemsw@gmail.com>
2021-07-30 13:53:16 -0700

    Starting to remove javascript in favor of css

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-window.onunload = function(){ window.scrollTo(0,0); }
-
-function scrollToDiv(div) {
-    $('html,body').animate({
-        scrollTop: $("#" + div).offset().top - 50},
-        'fast');
-}
